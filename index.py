5d1137f73469a3b1ea9bfd9481b66bb31254b55d
Louis Allard <la.devweb@yahoo.com>
2020-11-30 17:09:28 +0100

    index

--- index.py
+++ index.py
@@ -6,4 +6,12 @@ url = 'https://fr.wikipedia.org/wiki/Liste_des_pays_du_monde'
 response = requests.get(url)
 
 if response.ok:
-    print(response.text)
+    links = []
+    soup = BeautifulSoup(response.text, 'html.parser')
+    tds = soup.findAll('td')
+    for td in tds:
+        a = td.find('a')
+        link = a['href']
+        links.append('https://fr.wikipedia.org/wiki/Liste_des_pays_du_monde' + link)
+
+    print(links)
