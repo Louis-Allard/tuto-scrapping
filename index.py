f971599a6f18aa7bda81b74106687cf70cc815c9
Louis Allard <la.devweb@yahoo.com>
2020-11-30 17:57:50 +0100

    index

--- index.py
+++ index.py
@@ -2,10 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'https://fr.wikipedia.org/wiki/Liste_des_pays_du_monde'
-
 response = requests.get(url)
 
 if response.ok:
     soup = BeautifulSoup(response.text, 'html.parser')
     tds = soup.findAll('span', class_ = 'mw-headline')
+    
     [print(str(td.text) + '\n') for td in tds]
