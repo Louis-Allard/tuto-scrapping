c1224331fe8441aed520bcd572c58c36797d898e
Louis Allard <la.devweb@yahoo.com>
2020-12-01 11:49:52 +0100

    Scrap a list of pages

--- index.py
+++ index.py
@@ -2,22 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 
-'''
-code = 'fbc038'
-s_url = 'https://www.trenois.com/bosch-06019f200b-meuleuse-a-batterie-gws12v-76-article-'
-c_url = s_url + code
-'''
+for i in range(0,40):    
+    print('Page ' + str(i))
+    url = Request('https://www.trenois.com/bosch-06019f200b-meuleuse-a-batterie-gws12v-76-article-fbc00' + str(i), headers={'User-Agent': 'Mozilla/5.0'})
+    response = urlopen(url).read()      
 
-url = Request('https://www.trenois.com/bosch-06019f200b-meuleuse-a-batterie-gws12v-76-article-fbc0038', headers={'User-Agent': 'Mozilla/5.0'})
-response = urlopen(url).read()
-                    
+    soup = BeautifulSoup(response, 'html.parser')
 
-soup = BeautifulSoup(response, 'html.parser')
-title = soup.find('title')
-des = soup.find(id = 'designation')
-ref = soup.find('span', class_ = 'titreprincipal_titre-declinaison')
-car = soup.find('table', class_= 'table-caracteristiques')
-
-print(title.text + '\n\n' + ref.text[1::] + '\n\n' + des.text + '\n\n' + car.text)
-
-
+    title = soup.find('title')
+    
+    des = soup.find(id = 'designation')
+    ref = soup.find('span', class_ = 'titreprincipal_titre-declinaison')
+    car = soup.find('table', class_= 'table-caracteristiques')
+    
+    if len(title)!=0:  
+        print(title.text + '\n\n' + ref.text[1::] + '\n\n' + des.text + '\n\n' + car.text)
+    else:
+        print("Pas de page produit")
