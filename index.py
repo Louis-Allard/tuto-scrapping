66c3ef41b42a9df08313e016fb3a10f311de6cdd
Louis Allard <la.devweb@yahoo.com>
2020-12-01 10:43:02 +0100

    Add description

--- index.py
+++ index.py
@@ -1,13 +1,16 @@
 import requests 
 from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
 
-url = 'https://fr.wikipedia.org/wiki/Liste_des_pays_du_monde'
-response = requests.get(url)
+url = Request('https://www.trenois.com/bosch-06019f200b-meuleuse-a-batterie-gws12v-76-article-fbc0037', headers={'User-Agent': 'Mozilla/5.0'})
+response = urlopen(url).read()
 
-if response.ok:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    tds = soup.findAll('span', class_ = 'mw-headline')
-    
-    [print(str(td.text) + '\n') for td in tds]
-else:
-    print('Erreur de response : ' + str(response))
+soup = BeautifulSoup(response, 'html.parser')
+title = soup.find('title')
+des = soup.find(id = 'designation')
+ref = soup.find('span', class_ = 'titreprincipal_titre-declinaison')
+car = soup.find('table', class_= 'table-caracteristiques')
+
+print(title.text + '\n\n' + ref.text[1::] + '\n\n' + des.text + '\n\n' + car.text)
+
+
