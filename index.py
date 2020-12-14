cf60b46b5682e33555498e8592e85b59c4d01dbf
Louis Allard <la.devweb@yahoo.com>
2020-12-14 10:39:04 +0100

    Add env var.

--- index.py
+++ index.py
@@ -2,13 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import mysql.connector
+from data import HOST, USER, PASSWORD, DATABASE, URL
 
-bdd = mysql.connector.connect(host="", user="", password="", database="")
+bdd = mysql.connector.connect(host=HOST, user=USER, password=PASSWORD, database=DATABASE)
+
 curseur = bdd.cursor()
 
 for i in range(0,90):    
     print('Page ' + str(i))
-    url = Request('https://www.trenois.com/bosch-06019f200b-meuleuse-a-batterie-gws12v-76-article-fbc00' + str(i), headers={'User-Agent': 'Mozilla/5.0'})
+    url = Request(URL + str(i), headers={'User-Agent': 'Mozilla/5.0'})
     response = urlopen(url).read()      
 
     soup = BeautifulSoup(response, 'html.parser')
@@ -20,7 +22,6 @@ for i in range(0,90):
     car = soup.find('table', class_= 'table-caracteristiques')
     
     if len(title)!=0:  
-        #print(title.text + '\n\n' + ref.text[1::] + '\n\n' + des.text + '\n\n' + car.text)
         curseur.execute("INSERT INTO produits(REF, TITLE, DESCR, CARACT) VALUES (%s, %s, %s, %s)", (ref.text[12::], title.text, des.text, car.text ))
         bdd.commit()
     else:
@@ -28,4 +29,5 @@ for i in range(0,90):
 
 bdd.close()
 
+print(bdd)
 print("=================== END ===================")
